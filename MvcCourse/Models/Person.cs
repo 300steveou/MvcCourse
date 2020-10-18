d10e70f65a3627378e5d7ef6cd59b868b35db5dc
OU SHIH TUNG <300steveou@gmail.com>
2020-10-18 16:44:10 +0800

    加入displayname

--- MvcCourse/Models/Person.cs
+++ MvcCourse/Models/Person.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 
 namespace MvcCourse.Models
 {
@@ -6,11 +7,13 @@ namespace MvcCourse.Models
     {
         public int Id { get; set; }
 
+        [DisplayName("姓名")]
         [Required(ErrorMessageResourceName = "PersonName_Valid", ErrorMessageResourceType = typeof(Common))]
         public string Name { get; set; }
          
+        [DisplayName("年齡")]
         [Required(ErrorMessageResourceName = "Age_Valid", ErrorMessageResourceType = typeof(Common))]
-        [Range(18, 99, ErrorMessage = "請輸入年紀至少18歲")]
+        [Range(18, 99, ErrorMessage = "請輸入年紀介於{1}~{2}處之間")]
         public int Age { get; set; }
     }
 }
